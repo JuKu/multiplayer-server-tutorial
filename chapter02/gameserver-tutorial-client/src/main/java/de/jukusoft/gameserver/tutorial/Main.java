734c77874d35dac7d4d81fa495136b03aa26caf2
JuKu <kuenzel.justin@t-online.de>
2016-10-25 22:28:45 +0200

    added chapter02.

---
+++ chapter02/gameserver-tutorial-client/src/main/java/de/jukusoft/gameserver/tutorial/Main.java
@@ -0,0 +1,12 @@
+package de.jukusoft.gameserver.tutorial;
+
+/**
+ * Created by Justin on 25.10.2016.
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        //
+    }
+
+}
---
+++ chapter02/gameserver-tutorial-server/src/main/java/de/jukusoft/gameserver/tutorial/ServerMain.java
@@ -0,0 +1,12 @@
+package de.jukusoft.gameserver.tutorial;
+
+/**
+ * Created by Justin on 25.10.2016.
+ */
+public class ServerMain {
+
+    public static void main (String[] args) {
+        //
+    }
+
+}
